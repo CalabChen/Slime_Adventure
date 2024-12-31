9046224f105a05d1dce60262cd977ab5028eeb64
CalabChen <125740354+CalabChen@users.noreply.github.com>
2024-12-31 15:14:05 +0800

    esc弹出暂停菜单完成

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject menuXin;
+    public bool isStop = true;
+    public AudioSource music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuXin.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isStop)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                menuXin.SetActive(true);
+                isStop = false;
+                Time.timeScale=(0);// game stop
+                music.Pause();
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuXin.SetActive(false);
+            isStop = true;
+            Time.timeScale=(1);// game stop
+            music.Play();
+        }
+    }
+
+    public void Resume()
+    {
+        menuXin.SetActive(false);
+        isStop = true;
+        Time.timeScale = 1;
+        music.Play();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("ForestScene");
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("ChapterPickScene",LoadSceneMode.Single);
+        Time.timeScale = 1;
+    }
+}
